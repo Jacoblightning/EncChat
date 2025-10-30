a7edf51cd59df87b7bd747cb226d7e8dc0f44b33
Jacob Freeman <t00niyf5@duck.com>
2025-10-29 20:48:41 -0400

    feat: Auth improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,32 @@ import './App.css'
 function App() {
   const [logged_in, setLoggedIn] = useState(false)
 
+  function login(username: String, password: String) {
+    const response = await fetch('/api/auth/token', {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      // TODO: Url encoding on stuff
+      body: `username=${username}&password=${password}`
+    });
+
+    const json = response.json();
+
+    
+  }
+
   function login_handler(_formData: FormData) {
-    alert("Login Called");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    login(username, password);
   }
 
   async function register_handler(formData: FormData) {
+    const username = formData.get("username");
+    const password = formData.get("password");
+  
     const response = await fetch('/api/users/create', {
       method: 'POST',
       headers: {
@@ -17,16 +38,20 @@ function App() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        'username': formData.get("username"),
-        'password': formData.get("password"),
+        'username': username,
+        'password': password,
       })
     });
 
+    const json = await response.json();
+
     if (!response.ok) {
-      const error = await response.json();
       const message = error["detail"];
       alert(message);
+      return;
     }
+
+    login(username, password);
   } 
 
   if (!logged_in) {
